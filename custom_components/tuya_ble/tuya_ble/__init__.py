e9193b08d76c9ae1710c50971bdffbca9a76234b
Holchan <holchangomes@hotmail.com>
2024-12-07 04:03:02 -0300

    Revert "Fix typos in class names within Tuya BLE components. Updated 'AbstractTuyaBLEDeviceManager' to 'AbstaractTuyaBLEDeviceManager' in init.py, manager.py, and tuya_ble.py files. This change corrects the spelling for consistency across the codebase."
    This reverts commit 05ea651303509b3dd95b1d49b700192524499033.

--- custom_components/tuya_ble/tuya_ble/__init__.py
+++ custom_components/tuya_ble/tuya_ble/__init__.py
@@ -8,14 +8,14 @@ from .const import (
     TuyaBLEDataPointType, 
 )
 from .manager import (
-    AbstaractTuyaBLEDeviceManager,
+    AbstractTuyaBLEDeviceManager,
     TuyaBLEDeviceCredentials,
 )
 from .tuya_ble import TuyaBLEDataPoint, TuyaBLEDevice, TuyaBLEEntityDescription
 
 
 __all__ = [
-    "AbstaractTuyaBLEDeviceManager",
+    "AbstractTuyaBLEDeviceManager",
     "TuyaBLEDataPoint",
     "TuyaBLEDataPointType",
     "TuyaBLEDevice",
